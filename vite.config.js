982d8f21d1088a9eeab392f319b93c98adb2f3c4
Pronoy Roy <pronoy.roy2808@gmail.com>
2023-07-09 22:57:54 +0530

    changes in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/slite-clone',
   plugins: [react()],
   root: "src",
   server: {
